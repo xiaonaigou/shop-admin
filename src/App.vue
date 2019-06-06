cb98638496b220c0028dac35610ae28daffbb6e9
hcqq <1462353231@qq.com>
2019-06-06 12:36:26 +0800

    This is the first submission of vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    334
+
+    <el-button type="primary">主要按钮</el-button>
+
+    <p>
+      <router-view></router-view>
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  mounted(){
+
+    //请求测试接口
+    // axios({
+    //   url:"https://api.github.com/users",
+    //   method:"GET",
+    //   // get请求参数
+    //   params:{
+    //     abb:334
+    //   }
+    // }).then(res =>{
+    //   console.log(res);
+    // })
+
+    this.$axios({
+      url:"https://api.github.com/users",
+      method:"GET",
+    }).then(res =>{
+      console.log(res);
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios"
+import VueRouter from "vue-router"
 
-Vue.config.productionTip = false
+// element: 1.导入组件
+import ElementUI from "element-ui"
+//element样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 导入组件
+import Login from "./pages/Login.vue";
+
+// element: 2. 注册element插件
+Vue.use(ElementUI);
+
+// 注册路由
+Vue.use(VueRouter);
+
+// 配置路由
+const routes = [
+  { path:"/login",component: Login }
+];
+
+// 路由实例
+const router = new VueRouter({ routes });
+
+Vue.config.productionTip = false;
+
+//绑定到原型
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  // 挂载路由
+  router
+}).$mount('#app') //$mount绑定控制区域,相当于el
